5d0489bc1e18f5de3d24d46df0fde1aec1dcd46a
VladimirKataev <vova.kataev@gmail.com>
2022-07-18 09:30:07 -0700

    unindented. emacs has difficulty with this

--- frontend/frontEndScript.js
+++ frontend/frontEndScript.js
@@ -1,33 +1,33 @@
-      //for on-canvas positions, use vh
-      //horizontal use h
-      //vertical use v
-      //xyz are reserved for on-board positions
+//for on-canvas positions, use vh
+//horizontal use h
+//vertical use v
+//xyz are reserved for on-board positions
 
-      let zv = 40;
-      let zh = 0;
-      let xv = 15;
-      let xh = 25;
-      let yv = 15;
-      let yh = -15;
-      let ov = 200;
-      let oh = 200;
-
-
-
-      
-      let gfx = document.getElementById("gfx").getContext("2d");
+let zv = 40;
+let zh = 0;
+let xv = 15;
+let xh = 25;
+let yv = 15;
+let yh = -15;
+let ov = 200;
+let oh = 200;
 
 
-      
-      function testDraw(){
-	  gfx.moveTo(oh, ov);
-	  gfx.lineTo(10,10);
-	  gfx.stroke();
-      }
 
-      function gridToView(x, y, z){
-	  
-      }
 
-      testDraw();
+let gfx = document.getElementById("gfx").getContext("2d");
+
+
+
+function testDraw(){
+  gfx.moveTo(oh, ov);
+  gfx.lineTo(10,10);
+  gfx.stroke();
+}
+
+function gridToView(x, y, z){
+
+}
+
+testDraw();
 
