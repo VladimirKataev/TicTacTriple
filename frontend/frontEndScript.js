4beb951a3edf97d36240f24dbea4f3ba9f162a07
VladimirKataev <vova.kataev@gmail.com>
2022-07-18 10:17:54 -0700

    seeing progress

--- frontend/frontEndScript.js
+++ frontend/frontEndScript.js
@@ -17,17 +17,47 @@ let oh = 200;
 
 let gfx = document.getElementById("gfx").getContext("2d");
 
+//in: 0,1,2
+//out:123, 234, 345
+function gridCoordsToGFXCoords(x, y, z){
+    let ansV = (x * xv) + (y*yv) + (z * zv);
+    let ansH = (x * xh) + (y*yh) + (z * zh);
+    return {x: ansH + 200, y: ansV+200};
+}
 
+//to reset box origin relative to canvas center
+let translateObject = gridCoordsToGFXCoords(1,1,1);
+translateObject.x *= -1;
+translateObject.y *= -1;
+
+
+//pure on-canvas macro to simplify drawing
+function drawCircleAt(x, y, r){
+    gfx.beginPath();
+    gfx.arc(x, y, r, 0, 2*Math.PI);
+    gfx.strokeStyle = '#F00';
+    gfx.stroke();
+}
 
 function testDraw(){
-  gfx.moveTo(oh, ov);
-  gfx.lineTo(10,10);
-  gfx.stroke();
+    drawCircleAt(1,1,1);
+    drawCircleAt(2,2,2);
+    
+    console.log(gridCoordsToGFXCoords(0,0,0));
+    console.log(gridCoordsToGFXCoords(1,1,1));
+    console.log(gridCoordsToGFXCoords(2,2,0));
+    console.log(gridCoordsToGFXCoords(2,2,2));
+
+    let curr = gridCoordsToGFXCoords(0,0,0);
+    drawCircleAt(curr.x, curr.y, 20);
+    curr = gridCoordsToGFXCoords(1,1,1);
+    drawCircleAt(curr.x, curr.y, 20);
+    curr = gridCoordsToGFXCoords(2,2,2);
+    drawCircleAt(curr.x, curr.y, 20);
+    curr = gridCoordsToGFXCoords(2,2,0);
+    drawCircleAt(curr.x, curr.y, 20);
 }
 
-function gridToView(x, y, z){
-
-}
 
 testDraw();
 
