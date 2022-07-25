9349b429a762e43e80d42042f8dc9ee7c98a8e8d
VladimirKataev <vova.kataev@gmail.com>
2022-07-25 10:47:55 -0700

    prototype of game functionality done

--- backend/tictactriplegame.py
+++ backend/tictactriplegame.py
@@ -2,8 +2,8 @@
 
 class board:
     
-    attackVectors = [] #a list of all victory condition masks (49 32bit ints)
-
+    attackVectors = [7] #a list of all victory condition masks (49 32bit ints), rn its '7' because first one
+                        #
     def __init__(self):
         self.takenMask = 0  #32bit int on occupied spot
         self.redMask = 0    #32bit int on which spots are red
@@ -14,12 +14,26 @@ class board:
         
         while(self.takenMask & maskPos > 0):
             maskPos = maskPos << 9
-        takenMask = takenMask | maskPos
+        self.takenMask = self.takenMask | maskPos
         if(self.redTurn):
-            redMask = redMask | maskPos
+            self.redMask = self.redMask | maskPos
 
         self.redTurn = not self.redTurn
     
+    def checkGameEnd(self):
+        for dir in board.attackVectors:
+            if(dir & self.takenMask == dir): #the entire attack vector is full
+                if(dir & self.redMask == dir and not self.redTurn): #they're all red
+                    return True
+                elif(dir & self.redMask == 0 and self.redTurn): #they're all blue
+                    return True
 
+        return False
 
     
+
+tstGame = board()
+
+while(not tstGame.checkGameEnd()):
+    mv = int(input("Enter you move 0-8"))
+    tstGame.move(mv)
